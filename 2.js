8802a4d74646ca290b6336e622a995b345a6738f
Alena Karpenko <sunalena96@mail.ru>
2017-11-20 19:44:32 +0300

    2.js

--- 2.js
+++ 2.js
@@ -42,33 +42,3 @@ startList = function allclose() {
 		document.getElementById(id_menu[i]).style.display = "none";
 	}
 }
-function openMenu(id){
-	
-	for (i=0; i < 3; i++){
-		if (id != id_menu[i] &&( id == 'sub_menu_1' || id == 'sub_menu_2' || id == 'sub_menu_3' )){
-			document.getElementById(id_menu[i]).style.display = "none";
-		}
-	}
-    for (i=3; i < 6; i++){
-		if (id != id_menu[i] &&( id == 'sub_menu_4' || id == 'sub_menu_5' || id == 'sub_menu_6' )){
-			document.getElementById(id_menu[i]).style.display = "none";
-		}
-	}
-	for (i=6; i < 9; i++){
-		if (id != id_menu[i] &&( id == 'sub_menu_7' || id == 'sub_menu_8' || id == 'sub_menu_9' )){
-			document.getElementById(id_menu[i]).style.display = "none";
-		}
-	}
-	for (i=9; i < 12; i++){
-		if (id != id_menu[i] &&( id == 'sub_menu_11' || id == 'sub_menu_12' || id == 'sub_menu_13' )){
-			document.getElementById(id_menu[i]).style.display = "none";
-		}
-	}
-	if (document.getElementById(id).style.display == "block"){
-		document.getElementById(id).style.display = "none";
-	}else{
-		document.getElementById(id).style.display = "block";
-	}
-}
-window.onload=startList;
->>>>>>> 1b28f88e1b433819e5592ee0bbbc514a9854add1
